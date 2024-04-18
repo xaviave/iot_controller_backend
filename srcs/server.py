9d9052587f1f6fbe811316d5339baf74a1435368
GMX <hellxaviave@gmail.com>
2024-04-18 17:46:09 +0200

    update client/server

--- srcs/server.py
+++ srcs/server.py
@@ -13,8 +13,12 @@ category_request = products_controller_pb2.CategoryRequest(id=1, name="cate")
 category_response = products_controller_pb2.CategoryResponse(id=1, name="cate")
 
 # Create LedMode Object
-led_mode_request = products_controller_pb2.LedModeRequest(id=1, name="mode smth")
-led_mode_response = products_controller_pb2.LedModeResponse(id=1, name="mode smth")
+request = products_controller_pb2.ColorModeRequest(name="xavou", color="#1234df")
+response = products_controller_pb2.ColorModeResponse(name="xavou", color="#1234df")
+led_mode_request = products_controller_pb2.LedModeRequest()
+led_mode_request.ColorMode.CopyFrom(request)
+led_mode_response = products_controller_pb2.LedModeResponse()
+led_mode_response.ColorMode.CopyFrom(response)
 
 # Create CoffeeMachine Object
 coffee_machine_args = {
@@ -34,9 +38,12 @@ coffee_machine_args["categories"] = [category_response]
 coffee_machine_response = products_controller_pb2.CoffeeMachineResponse(**coffee_machine_args)
 
 # Create LedMode Object
-led_mode_request = products_controller_pb2.LedModeRequest(id=3, name="mode smth")
-led_mode_response = products_controller_pb2.LedModeResponse(id=3, name="mode smth")
-
+request = products_controller_pb2.ColorModeRequest(name="ihihih", color="#1234df")
+response = products_controller_pb2.ColorModeResponse(name="ihihih", color="#1234df")
+led_mode_request = products_controller_pb2.LedModeRequest()
+led_mode_request.ColorMode.CopyFrom(request)
+led_mode_response = products_controller_pb2.LedModeResponse()
+led_mode_response.ColorMode.CopyFrom(response)
 # Create LedPanel Object
 led_panel_args = {
     "id": 3,
@@ -53,14 +60,14 @@ led_panel_args["categories"] = [category_response]
 led_panel_response = products_controller_pb2.LedPanelResponse(**led_panel_args)
 
 base_product_1_request = products_controller_pb2.BaseProductRequest()
-base_product_1_request.coffee_machine.CopyFrom(coffee_machine_request)
+base_product_1_request.CoffeeMachine.CopyFrom(coffee_machine_request)
 base_product_1_response = products_controller_pb2.BaseProductResponse()
-base_product_1_response.coffee_machine.CopyFrom(coffee_machine_response)
+base_product_1_response.CoffeeMachine.CopyFrom(coffee_machine_response)
 
 base_product_2_request = products_controller_pb2.BaseProductRequest()
-base_product_2_request.led_panel.CopyFrom(led_panel_request)
+base_product_2_request.LedPanel.CopyFrom(led_panel_request)
 base_product_2_response = products_controller_pb2.BaseProductResponse()
-base_product_2_response.led_panel.CopyFrom(led_panel_response)
+base_product_2_response.LedPanel.CopyFrom(led_panel_response)
 
 project_date = datetime.now()
 request = products_controller_pb2.ProjectRequest(
