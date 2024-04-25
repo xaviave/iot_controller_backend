7c6e70e419c210a7ec3ed17f70c26dd50c96f1bb
Xavier Martin <xavierm@axis.com>
2024-04-25 16:25:14 +0200

    grpc django to iot
    Change-Id: I8d2fb470d9b67935bf4bb0c5d70ec585c1201396

--- srcs/features/products_controller/urls.py
+++ srcs/features/products_controller/urls.py
@@ -1,14 +1,6 @@
 from django.urls import path
 
-from .services.communication.brightness import BrightnessView
-from .services.communication.mode import ModeView
-from .services.communication.status import StatusView
-
 app_name = "products_controller"
 urlpatterns = [
-    # product communication functions
-    path("products/<int:pk>/status/<str:status>", StatusView.as_view(), name="status_view"),
-    path("products/<int:pk>/mode/<str:mode>", ModeView.as_view(), name="mode_view"),
-    path("products/<int:pk>/mode/<str:mode>/<int:mode_pk>", ModeView.as_view(), name="mode_view"),
-    path("products/<int:pk>/brightness", BrightnessView.as_view(), name="brightness_view"),
+    
 ]
