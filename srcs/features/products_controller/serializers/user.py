5b44172852227c2427a13c6d5b655a52a373623d
Xavier Martin <xavierm@axis.com>
2024-07-01 13:57:18 +0200

    linter
    Change-Id: I098c329d3711cf92e68b1998a6794cfed2b17705

--- srcs/features/products_controller/serializers/user.py
+++ srcs/features/products_controller/serializers/user.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.models import User
 from django_socio_grpc import proto_serializers
-from rest_framework import serializers
-
 from features.products_controller.grpc.products_controller_pb2 import (
     UserListResponse,
     UserResponse,
 )
+from rest_framework import serializers
 
 
 class UserSerializer(proto_serializers.ModelProtoSerializer):
