9ad70f89dfc63a5f5bc59b9243493077e8cea6b8
Xavier Martin <xavierm@axis.com>
2024-07-25 16:03:03 +0200

    update proto + serializer
    Change-Id: I69086ff343c405ed8d17b6dccc9a2e6a98e126f8

--- srcs/features/products_controller/serializers/celery_tasks/crontab_schedule.py
+++ srcs/features/products_controller/serializers/celery_tasks/crontab_schedule.py
@@ -1,3 +1,5 @@
+from zoneinfo import ZoneInfo
+
 from django_celery_beat.models import CrontabSchedule
 from django_socio_grpc import proto_serializers
 from features.products_controller.grpc.products_controller_pb2 import (
@@ -13,3 +15,14 @@ class CrontabScheduleSerializer(proto_serializers.ModelProtoSerializer):
 
         proto_class = CrontabScheduleResponse
         proto_class_list = CrontabScheduleListResponse
+
+    def to_representation(self, instance):
+        d = super().to_representation(instance)
+        # IANA time zone database as String
+        d["timezone"] = instance.timezone.key
+        return d
+
+    def to_internal_value(self, data):
+        # IANA time zone database as String
+        data["timezone"] = ZoneInfo(key=data["timezone"])
+        return super().to_internal_value(data)
--- srcs/features/products_controller/serializers/celery_tasks/periodic_task.py
+++ srcs/features/products_controller/serializers/celery_tasks/periodic_task.py
@@ -1,31 +1,30 @@
 from django_celery_beat.models import PeriodicTask
 from django_socio_grpc import proto_serializers
 from features.products_controller.grpc.products_controller_pb2 import PeriodicTaskListResponse, PeriodicTaskResponse
-from features.products_controller.models.products.base_product import BaseProduct
-from features.products_controller.models.project import Project
-from rest_framework import serializers
+from features.products_controller.serializers.celery_tasks.clocked_schedule import ClockedScheduleSerializer
+from features.products_controller.serializers.celery_tasks.crontab_schedule import CrontabScheduleSerializer
+from features.products_controller.serializers.celery_tasks.interval_schedule import IntervalScheduleSerializer
+from features.products_controller.serializers.celery_tasks.solar_schedule import SolarScheduleSerializer
 
 
 class PeriodicTaskSerializer(proto_serializers.ModelProtoSerializer):
-    name = serializers.CharField()
-    task = serializers.CharField()
-
-    class_type = serializers.CharField()
-    class_id = serializers.CharField()
+    clocked = ClockedScheduleSerializer()
+    crontab = CrontabScheduleSerializer()
+    interval = IntervalScheduleSerializer()
+    solar = SolarScheduleSerializer()
 
     class Meta:
         model = PeriodicTask
-        fields = ["name", "task", "class_type", "class_id", "interval", "crontab", "solar", "clocked"]
+        fields = ["name", "task", "kwargs", "clocked", "crontab", "interval", "solar"]
 
         proto_class = PeriodicTaskResponse
         proto_class_list = PeriodicTaskListResponse
 
     def create(self, validated_data):
-        classes = {"Project": Project, "BaseProduct": BaseProduct}
-        class_type = classes[validated.pop("class_type")]
-        obj = class_type.objects.get(id=validated_data.pop("class_id"))
+        # classes = {"Project": Project, "BaseProduct": BaseProduct}
+        # class_type = classes[kwargs.pop("class_type")]
+        # obj = class_type.objects.get(id=kwargs.pop("class_id"))
 
-        validated_data["args"] = {"obj": obj}
         validated_data.task = f"srcs.features.products_controller.tasks.{validated_data['task']}"
 
         instance = PeriodicTask.objects.create(**validated_data)
